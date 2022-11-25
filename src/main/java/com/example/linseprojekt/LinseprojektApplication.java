11130a3f2625a914819e357d7c68e1ca2624beb0
nann6665 <nann6665@kea.stud.dk>
2022-11-25 10:48:25 +0100

    test

---
+++ src/main/java/com/example/linseprojekt/LinseprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.linseprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinseprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinseprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/linseprojekt/LinseprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linseprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinseprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
